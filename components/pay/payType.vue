ff71c77e2c5d5cc6b4d751b85a009380192f5152
wuhan <624396832@qq.com>
2021-10-27 10:16:35 +0800

    提交

---
+++ components/pay/payType.vue
@@ -0,0 +1,41 @@
+<template>
+  <view class="payType">
+    <u-mask :show="show" @click="show = false" @click="close">
+      <view class="warp">
+        <view class="rect" @tap.stop></view>
+      </view>
+    </u-mask>
+  </view>
+</template>
+
+<script>
+export default {
+  name: "payType",
+  props:['show'],
+  data(){
+    return{
+      // show:true
+    }
+  },
+  methods:{
+    close(){
+      console.log(1);
+      this.$emit('close')
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.warp {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  .rect {
+    width: 120px;
+    height: 120px;
+    background-color: #fff;
+  }
+}
+</style>
