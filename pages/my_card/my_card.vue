0d243da04423cc3cd136eb761ac405cf08dfe4b3
chengdan <905702879@qq.com>
2021-10-27 14:57:06 +0800

    我的卡卷页面

--- pages/my_card/my_card.vue
+++ pages/my_card/my_card.vue
@@ -1,6 +1,46 @@
 <template>
-	<view>
-		
+	<view class="container">
+		<view class="titles">
+			<text :class="current==0?'active':''" @click="change(0)">订座套餐卡</text>
+			<text :class="current==1?'active':''" @click="change(1)">预订时长卡</text>
+		</view>
+		<view class="card-box" v-show="current==0">
+			<view class="card-list">
+				<text class="tag u-font-23">春深券</text>
+				<view class="info">
+					<view class="u-font-32 u-bold">两小时免费自习体验券</view>
+					<view class="u-font-24 data">2021年09月15日到期</view>
+				</view>
+				<view class="times">
+					<view class="u-font-24">剩余：3小时0分钟（1天）</view>
+					<view class="use u-font-23">去使用</view>
+				</view>
+			</view>
+			<view class="card-list">
+				<text class="tag u-font-23">春深券</text>
+				<view class="info">
+					<view class="u-font-32 u-bold">两小时免费自习体验券</view>
+					<view class="u-font-24 data">2021年09月15日到期</view>
+				</view>
+				<view class="times">
+					<view class="u-font-24">剩余：3小时0分钟（1天）</view>
+					<view class="use u-font-23">去使用</view>
+				</view>
+			</view>
+		</view>
+		<view class="card-box" v-show="current==1">
+			<view class="card-list">
+				<text class="tag u-font-23">春深券</text>
+				<view class="info">
+					<view class="u-font-32 u-bold">两小时免费自习体验券</view>
+					<view class="u-font-24 data">2021年09月15日到期</view>
+				</view>
+				<view class="times">
+					<view class="u-font-24">剩余：3小时0分钟（1天）</view>
+					<view class="use u-font-23">去使用</view>
+				</view>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -8,15 +48,91 @@
 	export default {
 		data() {
 			return {
-				
+				current:0
 			}
 		},
 		methods: {
-			
+			change(index){
+				this.current = index;
+			}
 		}
 	}
 </script>
 
-<style>
+<style scoped lang="scss">
+	.container{
+		background-color: #F5F5F5;
+	}
+	.titles{
+		background-color: #FFFFFF;
+		display: flex;
+		justify-content: space-evenly;
+		padding: 78rpx 0 32rpx 0;
+				
+		text{
+			font-size: 31rpx;
+			color: #010101;
+		}
+		text.active{
+			position: relative;
+			color: #2487FF;
+		}
+		text.active::before{
+			content: '';
+			display: inline-block;
+			position: absolute;
+			bottom: -15rpx;
+			left: 50%;
+			transform: translateX(-50%);
+			background-color: #2487FF;
+			border-radius: 4rpx;
+			width: 48rpx;
+			height: 8rpx;
+		}
+	}
+	.card-box{
+		padding: 26rpx 30rpx;
+		
+		.card-list{
+			padding: 20rpx 30rpx;
+			background-color: #FFFFFF;
+			border-radius: 19rpx;
+			margin-bottom: 25rpx;
+			
+			.tag{
+				display: inline-block;
+				background: #2487FF;
+				border-radius: 6rpx;
+				color: #FFFFFF;
+				height: 31rpx;
+				line-height: 31rpx;
+				padding: 0 12rpx;
+			}
+			.info{
+				border-bottom: 2rpx dashed #8B8B8B;
+				padding: 22rpx 0;
+				.data{
+					color: #666666;
+					margin-top: 20rpx;
+				}
+			}
+			.times{
+				display: flex;
+				justify-content: space-between;
+				align-items: center;
+				color: #666666;
+				padding-top: 10rpx;
+				.use{
+					width: 122rpx;
+					height: 51rpx;
+					background: #EB5940;
+					border-radius: 26rpx;
+					color: #FFFFFF;
+					text-align: center;
+					line-height: 51rpx;
+				}
+			}
+		}
+	}
 
 </style>
--- pages/my_seat/detail/detail.vue
+++ pages/my_seat/detail/detail.vue
@@ -1,6 +1,26 @@
 <template>
-	<view>
-		
+	<view class="container">
+		<view class="table">
+			<u-table :font-size="23">
+				<u-tr>
+					<u-th width="178rpx">日期</u-th>
+					<u-th>座位号</u-th>
+					<u-th width="178rpx">订座时间</u-th>
+					<u-th>时长</u-th>
+					<u-th>价格</u-th>
+				</u-tr>
+				<block v-for="item in list">
+					<u-tr>
+						<u-td width="178rpx">{{item.data}}</u-td>
+						<u-td>{{item.number}}</u-td>
+						<u-td width="178rpx">{{item.time}}</u-td>
+						<u-td>{{item.duration}}小时</u-td>
+						<u-td>{{item.price}}元</u-td>
+					</u-tr>
+				</block>
+			</u-table>
+		</view>
+		<view class="all-time u-font-28">合计时长：<text class="u-black-color">48小时</text></view>
 	</view>
 </template>
 
@@ -8,15 +28,47 @@
 	export default {
 		data() {
 			return {
-				
+				list:[
+					{
+						data:'20201-09-08',
+						number:'1',
+						time:'18:00-24:00',
+						duration:'16',
+						price:'18'
+					},
+					{
+						data:'20201-09-012',
+						number:'4',
+						time:'18:00-24:00',
+						duration:'6',
+						price:'66'
+					},
+					{
+						data:'20201-10-08',
+						number:'7',
+						time:'18:00-24:00',
+						duration:'6',
+						price:'68'
+					},
+				]
 			}
 		},
 		methods: {
-			
+
 		}
 	}
 </script>
 
-<style>
-
+<style scoped lang="scss">
+	.container{
+		padding: 20rpx 30rpx;
+	}
+	.table{
+		background-color: #FFFFFF;
+		padding-bottom: 22rpx;
+	}
+	.all-time{
+		text-align: right;
+		color: #8D8D8D;
+	}
 </style>
